f78dbc3f52693c5e6b9886a46cdc471302e3b0d0
Tyler Farrior <tylerfarrior@gmail.com>
2023-09-04 10:17:14 -0400

    set up id generator

--- server.js
+++ server.js
@@ -13,63 +13,60 @@ app.use(express.static('public'));
 
 // HTML Routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // API Routes
 app.get('/api/notes', (req, res) => {
-  // Read the notes from the db.json file and send them as JSON
-  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8'));
-  res.json(notes);
+    // Read the notes from the db.json file and send them as JSON
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8'));
+    console.log(notes)
+    res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
-  const newNote = req.body;
-  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8'));
+    const newNote = req.body;
+    const notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
 
-  // Generate a unique ID for the new note (you can use a package like 'uuid' for this)
-  newNote.id = generateUniqueId();
+    // Generate a unique ID for the new note (you can use a package like 'uuid' for this)
+    // Function to generate a unique ID (for example, using the 'uuid' package)
+    newNote.id = Math.floor(Math.random() * 100000000000)
 
-  // Add the new note to the array of notes
-  notes.push(newNote);
-
-  // Write the updated notes back to the db.json file
-  fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes, null, 2));
-
-  res.json(newNote);
-});
-
-// Bonus: Delete Route
-app.delete('/api/notes/:id', (req, res) => {
-  const noteId = req.params.id;
-  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8'));
-
-  // Find the index of the note with the given id
-  const noteIndex = notes.findIndex((note) => note.id === noteId);
-
-  if (noteIndex !== -1) {
-    // Remove the note from the array
-    notes.splice(noteIndex, 1);
+    // Add the new note to the array of notes
+    notes.push(newNote);
 
     // Write the updated notes back to the db.json file
     fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes, null, 2));
 
-    res.sendStatus(204); // No Content
-  } else {
-    res.sendStatus(404); // Not Found
-  }
+    res.json(newNote);
+});
+
+// Bonus: Delete Route
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8'));
+
+    // Find the index of the note with the given id
+    const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+    if (noteIndex !== -1) {
+        // Remove the note from the array
+        notes.splice(noteIndex, 1);
+
+        // Write the updated notes back to the db.json file
+        fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes, null, 2));
+
+        res.sendStatus(204); // No Content
+    } else {
+        res.sendStatus(404); // Not Found
+    }
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
-
-// Function to generate a unique ID (for example, using the 'uuid' package)
-function generateUniqueId() {
-  // Implement your logic to generate a unique ID here
-}
